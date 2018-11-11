9a947f14e1b6cc8e85697dd4cba9f299a1559897
zhouliujun <1096432931@qq.com>
2018-11-11 11:28:44 +0800

    更新mongoDB工具类 增加输出指定字段和分页查询

--- src/lib/mongoUtil.js
+++ src/lib/mongoUtil.js
@@ -23,16 +23,16 @@ class MongoDBTool {
      * 链接数据库与关闭数据库的方法
      * 参照这个格式： mongoose.connect('mongodb://user:password@127.0.0.1:27017/dbname')， 其中dbname是必选项， 不写会报错。 user和password可选。
      * @param {*} callback
-     * @param {*} database 库名 不传则使用默认配置
+     * @param {*} dbName 库名 不传则使用默认配置
      */
-    connect(callback, database) {
+    connect(callback, dbName) {
         MongoClient.connect(DB.mongoConf.host, {
             useNewUrlParser: true //
         }, (err, client) => {
             assert.strictEqual(null, err);
             console.log(`${DB.mongoConf.host} 数据库连接成功!!!`);
             //拿到数据库
-            const db = client.db(database || DB.mongoConf.database);
+            const db = client.db(dbName || DB.mongoConf.database);
             //操作数据
             callback(err, db);
             //关闭数据库
@@ -42,126 +42,133 @@ class MongoDBTool {
 
     /**
      * 增
-     * @param {* string} collectionName  表名
+     * @param {* string} table  表名
      * @param {* arr or obj} data  数据
-     * @param {*} database 库名
+     * @param {*} dbName 库名
      * @return 结果
      */
-    insert(collectionName, data, database) {
-        if (typeof collectionName != 'string') throw new Error('请检查传入的参数是否正确');
+    insert({
+        table,
+        data,
+        dbName
+    }) {
+        if (typeof table != 'string') throw new Error('请检查传入的参数是否正确');
         return new Promise((resolve, reject) => {
-            this.connect((err, db) => {
+            this.connect(async(err, db) => {
                 if (err) reject(err);
                 //取到想要的表
-                const collection = db.collection(collectionName);
+                const collection = db.collection(table);
                 if (Array.isArray(data)) { //判断数据是否是数组
                     //插入多条数据
-                    collection.insertMany(data).then(({
-                        result
-                    }) => {
-                        console.log(`插入${result.n}条数据`, result);
-                        resolve(result.n);
-                    });
+                    const result = await collection.insertMany(data);
+                    console.log(`插入${result.n}条数据`, result);
+                    resolve(result.n);
                 } else {
                     //插入一条数据
-                    collection.insertOne(data).then(({
-                        result
-                    }) => {
-                        console.log(`插入${result.n}条数据`, result);
-                        resolve(result.n);
-                    });
+                    const result = await collection.insertOne(data);
+                    console.log(`插入${result.n}条数据`, result);
+                    resolve(result.n);
                 }
-            }, database);
+            }, dbName);
         });
     }
 
     /**
      * 删除一条
-     * @param {* string} collectionName 集合名
-     * @param {* obj} condition 删除的条件
-     * @param {*} database 库名
+     * @param {* string} table 集合名
+     * @param {* obj} filters 删除的条件
+     * @param {*} dbName 库名
      * @return {* function} 返回结果
      */
-    deleteOne(collectionName, condition, database) {
-        if (typeof collectionName != 'string') throw new Error('请检查传入的参数是否正确');
+    deleteOne({
+        table,
+        filters,
+        dbName
+    }) {
+        if (typeof table != 'string') throw new Error('请检查传入的参数是否正确');
         return new Promise((resolve, reject) => {
-            this.connect((err, db) => {
+            this.connect(async(err, db) => {
                 if (err) reject(err);
-                const collection = db.collection(collectionName);
-                collection.deleteOne(condition).then(({
-                    result
-                }) => {
-                    console.log(`删除${result.n}条数据`, result);
-                    resolve(result.n);
-                });
-            }, database);
+                const collection = db.collection(table);
+                const result = await collection.deleteOne(filters);
+                console.log(`删除${result.n}条数据`, result);
+                resolve(result.n);
+            }, dbName);
         });
     }
 
     /**
      * 删除多条
-     * @param {* string} collectionName 集合名
-     * @param {* obj} condition 删除的条件
-     * @param {*} database 库名
+     * @param {* string} table 集合名
+     * @param {* obj} filters 删除的条件
+     * @param {*} dbName 库名
      * @return {*} 返回结果
      */
-    deleteMany(collectionName, condition, database) {
-        if (typeof collectionName != 'string') throw new Error('请检查传入的参数是否正确');
+    deleteMany({
+        table,
+        filters,
+        dbName
+    }) {
+        if (typeof table != 'string') throw new Error('请检查传入的参数是否正确');
         return new Promise((resolve, reject) => {
-            this.connect((err, db) => {
+            this.connect(async(err, db) => {
                 if (err) reject(err);
-                const collection = db.collection(collectionName);
-                collection.deleteMany(condition).then(({
-                    result
-                }) => {
-                    console.log(`删除${result.n}条数据`, result);
-                    resolve(result.n);
-                });
-            }, database);
+                const collection = db.collection(table);
+                const result = await collection.deleteMany(filters);
+                console.log(`删除${result.n}条数据`, result);
+                resolve(result.n);
+            }, dbName);
         });
     }
 
     /**
      * 修改一条
-     * @param {* string} collectionName  集合名
+     * @param {* string} table  集合名
+     * @param {* obj} filters 更新的条件
      * @param {* obj} data  更新数据
-     * @param {* obj} condition 更新的条件
-     * @param {*} database 库名
+     * @param {*} dbName 库名
      * @return { Number} 返回结果
      */
-    upadteOne(collectionName, condition, data, database) {
-        if (typeof collectionName != 'string' && arguments.length === 3) throw new Error('请检查传入的参数是否正确');
+    upadteOne({
+        table,
+        filters,
+        data,
+        dbName
+    }) {
+        if (typeof table != 'string' && arguments.length === 3) throw new Error('请检查传入的参数是否正确');
         return new Promise((resolve, reject) => {
-            this.connect((err, db) => {
+            this.connect(async(err, db) => {
                 if (err) reject(err);
-                const collection = db.collection(collectionName);
-                collection.updateOne(condition, {
+                const collection = db.collection(table);
+                const result = await collection.updateOne(filters, {
                     $set: data
-                }).then(({
-                    result
-                }) => {
-                    console.log(`修改${result.n}条数据`, result);
-                    resolve(result.n);
                 });
-            }, database);
+                console.log(`修改${result.n}条数据`, result);
+                resolve(result.n);
+            }, dbName);
         });
     }
 
     /**
      * 改多条
-     * @param {* string} collectionName  集合名
+     * @param {* string} table  集合名
+     * @param {* obj} filters 更新的条件
      * @param {* obj} data  更新数据
-     * @param {* obj} condition 更新的条件
-     * @param {*} database 库名
-     * @return {Number}  返回结果
+     * @param {*} dbName 库名
+     * @return { Number} 返回结果
      */
-    updateMany(collectionName, condition, data, database) {
-        if (typeof collectionName != 'string' && arguments.length === 3) throw new Error('请检查传入的参数是否正确');
+    updateMany({
+        table,
+        filters,
+        data,
+        dbName
+    }) {
+        if (typeof table != 'string' && arguments.length === 3) throw new Error('请检查传入的参数是否正确');
         return new Promise((resolve, reject) => {
             this.connect((err, db) => {
                 if (err) reject(err);
-                const collection = db.collection(collectionName);
-                collection.updateMany(condition, {
+                const collection = db.collection(table);
+                collection.updateMany(filters, {
                     $set: data
                 }).then(({
                     result
@@ -169,51 +176,66 @@ class MongoDBTool {
                     console.log(`修改${result.n}条数据`, result);
                     resolve(result.n);
                 });
-            }, database);
+            }, dbName);
         });
     }
 
     /**
      * 查询数据
-     * @param {* string} collectionName  表名
-     * @param {* obj} condition  条件
-     * @param {*} database 库名
+     * @param {* string} table  表名
+     * @param {* obj} filters  条件
+     * @param {*} attributes 输出指定列
+     * @param {*} dbName 库名
      * @return {* Array} 返回结果集列表
      */
-    findAll(collectionName, condition, database) {
-        if (typeof collectionName != 'string') throw new Error('请检查传入的参数是否正确');
+    findAll({
+        table,
+        filters,
+        attributes,
+        dbName
+    }) {
+        if (typeof table != 'string') throw new Error('请检查传入的参数是否正确');
         return new Promise((resolve, reject) => {
             this.connect((err, db) => {
                 if (err) reject(err);
-                const collection = db.collection(collectionName);
-                collection.find(condition).toArray((err, result) => {
+                const collection = db.collection(table);
+                collection.find(filters, {
+                    projection: attributes
+                }).toArray((err, result) => {
                     assert.strictEqual(err, null);
                     console.log(`查询数据${result.length}条数据`, result);
                     resolve(result);
                 });
-            }, database);
+            }, dbName);
         });
     }
 
     /**
      * 查询数据
      * @param {*} collectionName
-     * @param {*} condition
+     * @param {*} filters
+     * @param {*} attributes 输出指定列
      * @param {*} database 库名
      * @return {Object} 返回结果对象
      */
-    findOne(collectionName, condition, database) {
-        if (typeof collectionName != 'string') throw new Error('请检查传入的参数是否正确');
+    findOne({
+        table,
+        filters,
+        attributes,
+        dbName
+    }) {
+        if (typeof table != 'string') throw new Error('请检查传入的参数是否正确');
         return new Promise((resolve, reject) => {
-            this.connect((err, db) => {
+            this.connect(async(err, db) => {
                 if (err) reject(err);
-                const collection = db.collection(collectionName);
-                collection.findOne(condition).then((result) => {
-                    assert.strictEqual(err, null);
-                    console.log(`查询数据`, result);
-                    resolve(result);
+                const collection = db.collection(table);
+                const result = await collection.findOne(filters, {
+                    projection: attributes
                 });
-            }, database);
+                assert.strictEqual(err, null);
+                console.log(`查询数据`, result);
+                resolve(result);
+            }, dbName);
         });
     }
 
@@ -223,22 +245,64 @@ class MongoDBTool {
      * @param {*} database 库名
      * @return {* Number} 返回数量
      */
-    getAllCount(collectionName, database) {
-        if (typeof collectionName != 'string') throw new Error('请检查传入的参数是否正确');
+    getAllCount({
+        table,
+        filters,
+        dbName
+    }) {
+        if (typeof table != 'string') throw new Error('请检查传入的参数是否正确');
         return new Promise((resolve, reject) => {
-            this.connect((err, db) => {
+            this.connect(async(err, db) => {
                 if (err) reject(err);
                 //取到想要的表
-                const collection = db.collection(collectionName);
-                collection.count({}).then(({
-                    result
-                }) => {
-                    console.log(`查询数据${result.n}条数据`, result);
-                    resolve(result.n);
-                });
-            }, database);
+                const collection = db.collection(table);
+                const result = await collection.count(filters);
+                console.log(`查询数据${result}条数据`, result);
+                resolve(result);
+            }, dbName);
         });
     }
+
+    /**
+     * 分页查询数据
+     * @param {* string} table  表名
+     * @param {* obj} filters  条件
+     * @param {*} dbName 库名
+     * @param {*} attributes 输出指定列
+     * @param {*} page 分页参数 当前索引
+     * @param {*} limit 每页大小
+     * @return {* Array} 返回结果集列表
+     */
+    findAndCount({
+        table,
+        filters,
+        attributes,
+        dbName,
+        page,
+        limit
+    }) {
+        if (typeof table != 'string') throw new Error('请检查传入的参数是否正确');
+        return new Promise((resolve, reject) => {
+            this.connect(async(err, db) => {
+                if (err) reject(err);
+                const collection = db.collection(table);
+                let queryData = {
+                        projection: attributes
+                    }
+                    //分页查询
+                if (page && limit) {
+                    queryData.skip = Number((page - 1) * limit);
+                    queryData.limit = Number(limit);
+                }
+                collection.find(filters, queryData).toArray((err, result) => {
+                    assert.strictEqual(err, null);
+                    console.log(`分页查询数据${result.length}条数据`, result);
+                    resolve(result);
+                });
+            }, dbName);
+        });
+    }
+
 }
 
 //导出对象
