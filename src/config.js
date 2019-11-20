305b5dd7a5535aa004620fe9c681d72f0e496da7
zhouliujun <1096432931@qq.com>
2019-11-20 17:50:35 +0800

    更新配置文件

--- src/config.js
+++ src/config.js
@@ -3,10 +3,10 @@ import UUID from 'uuid';
 // 系统配置
 export const System = {
     API_server_type: 'http://', // API服务器协议类型,包含"http://"或"https://"
-    API_server_host: 'api.hao2013.cn', // API服务器暴露的域名地址,请勿添加"http://"
+    API_server_host: 'localhost', // API服务器暴露的域名地址,请勿添加"http://"
     API_server_port: '3000', // API服务器监听的端口号
     HTTP_server_type: 'http://', // HTTP服务器协议类型,包含"http://"或"https://"
-    HTTP_server_host: 'www.hao2013.cn', // HTTP服务器地址,请勿添加"http://" （即前端调用使用的服务器地址，如果是APP请设置为 * ）
+    HTTP_server_host: 'localhost', // HTTP服务器地址,请勿添加"http://" （即前端调用使用的服务器地址，如果是APP请设置为 * ）
     HTTP_server_port: '65534', // HTTP服务器端口号
     System_country: 'zh-cn', // 所在国家的国家代码
     System_plugin_path: path.join(__dirname, './plugins'), // 插件路径
@@ -55,10 +55,10 @@ export const DB = {
         /*  {
              DB_type: 'mysql', // 数据库类型
              dbName: 'webDB', //命名为唯一 调用的时候
-             host: '192.168.0.220', // 服务器地址
+             host: '192.168.0.118', // 服务器地址
              port: 3306, // 数据库端口号
              username: 'root', // 数据库用户名
-             password: 'scrh123456', // 数据库密码
+             password: '123456', // 数据库密码
              database: 'web_test_db', // 数据库名称
              //prefix: 'api_', // 默认"api_"
              dialectOptions: { // MySQL > 5.5，其它数据库删除此项
